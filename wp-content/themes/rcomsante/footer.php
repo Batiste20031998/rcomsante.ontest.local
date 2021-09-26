0a5bfdbe6e52526c26adff75c42fc231cba7c8d0
Batiste20031998 <batiste.blactot@gmail.com>
2021-09-26 18:04:17 +0200

    css navbar

--- wp-content/themes/rcomsante/footer.php
+++ wp-content/themes/rcomsante/footer.php
@@ -1,23 +1,36 @@
-<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <?php
-        wp_nav_menu(array(
-            'theme_location'    => 'footer',
-            'depth'             => 2,
-            'container'         => 'div',
-            'container_class'   => 'collapse navbar-collapse',
-            'container_id'      => 'bs-example-navbar-collapse-1',
-            'menu_class'        => 'nav navbar-nav',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new WP_Bootstrap_Navwalker(),
-        ));
-        ?>
+<footer>
+    <div class="social-footer">
+        <p class="my-0 ms2">Nous suivre</p>
+        <a class="ms-2" href="https://twitter.com/RComSante"><img src="<?= get_template_directory_uri() . '/img/RComSante-twitter-icon.svg'; ?>" alt=""></a>
+        <p class="my-0 ms-2">|</p>
+        <a class="ms-2" href="https://www.linkedin.com/company/rcs-r%C3%A9seau-communication-sant%C3%A9/"><img src="<?= get_template_directory_uri() . '/img/RComSante-linkedin-icon.svg' ?>" alt=""></a>
     </div>
-</nav>
+    <nav class="navbar navbar-expand-md navbar-light bg-white" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <?php
+            wp_nav_menu(array(
+                'theme_location'    => 'footer',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker(),
+            ));
+            ?>
+        </div>
+    </nav>
+    <div>
+        <p class="my-0 ms-2">© 2021 rComSanté - Tous droits réservés</p>
+    </div>
+
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 
--- wp-content/themes/rcomsante/inc/wp_enqueue_scripts.php
+++ wp-content/themes/rcomsante/inc/wp_enqueue_scripts.php
@@ -5,16 +5,15 @@ function montheme_register_assets()
 {
     wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css');
     wp_register_style('slick', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
-    wp_register_style('main', get_template_directory_uri() . '/css/main.css');
     wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
     wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js', [], false, true);
     wp_register_script('slick', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', ['jquery'], false, true);
     wp_deregister_script('jquery');
     wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.slim.min.js', [], false, true);
     wp_register_script('main', get_template_directory_uri() . '/js/main.js', [], false, true);
+    wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_style('bootstrap');
     wp_enqueue_style('slick');
-    wp_enqueue_style('main');
     wp_enqueue_script('bootstrap');
     wp_enqueue_script('slick');
     wp_enqueue_script('main');
